844e3ef8042715346f143c5e34c0706b07956f9e
Clinton R. Nixon <clinton.nixon@viget.com>
2009-03-15 17:12:34 -0400

    added to PDFMaker

--- src/scala/PDFMaker.scala
+++ src/scala/PDFMaker.scala
@@ -1,9 +1,24 @@
 import java.io._
 import scala.io.Source
 import org.xhtmlrenderer.pdf.ITextRenderer
+import javax.xml.parsers.{DocumentBuilder,DocumentBuilderFactory}
 
-class PDFMaker(val html: String) {
-  def this(htmlFile: File) = {
-    this(Source.fromFile(htmlFile).getLines.toList.reduceLeft((a, b) => a + b))
+object PDFMaker {
+  val documentBuilder = DocumentBuilderFactory.newInstance().newDocumentBuilder()
+}
+
+class PDFMaker(val fileName: String) {
+  val file = new File(fileName)
+
+  def text = Source.fromFile(file).getLines.toList.reduceLeft((a, b) => a + b)
+  def baseURL = new File(fileName).toURI().toURL().toString()
+  def document = PDFMaker.documentBuilder.parse(text)
+
+  def createPDF(outputFileName: String) {
+    val outputStream = new FileOutputStream(outputFileName)
+    val renderer = new ITextRenderer
+    renderer.setDocument(document, baseURL)
+    renderer.layout
+    renderer.createPDF(outputStream)
   }
 }
