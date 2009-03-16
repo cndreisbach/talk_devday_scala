02662246c491070722dd635c8fb904fd048c2348
Clinton R. Nixon <crnixon@gmail.com>
2009-03-16 14:54:48 -0400

    renamed to pdf_publisher

--- Buildfile
+++ Buildfile
@@ -8,12 +8,12 @@ define 'pdf-maker' do
   
   fs_url = 'http://pigeonholdings.com/projects/flyingsaucer/downloads/r8/flyingsaucer-R8rc1.zip'
   fs_zip = download('target/fs.zip'=>fs_url)
-  renderer_jar = file('target/xhtml_renderer/core-renderer.jar' => 
-    unzip('target/xhtml_renderer'=>fs_zip))
-  itext_jar = file('target/xhtml_renderer/iText-2.0.8.jar' =>
-    unzip('target/xhtml_renderer'=>fs_zip))
-  minium_jar = file('target/xhtml_renderer/minium.jar' => 
-    unzip('target/xhtml_renderer'=>fs_zip))
-  
+  renderer_jar = file('target/fs/core-renderer.jar' => 
+    unzip('target/fs'=>fs_zip))
+  itext_jar = file('target/fs/iText-2.0.8.jar' =>
+    unzip('target/fs'=>fs_zip))
+  minium_jar = file('target/fs/minium.jar' => 
+    unzip('target/fs'=>fs_zip))
+
   compile.with renderer_jar, itext_jar, minium_jar
 end
---
+++ src/main/scala/PDFPublisher.scala
@@ -0,0 +1,36 @@
+import java.io._
+import scala.io.Source
+import scala.collection.mutable.ArrayStack
+import org.xhtmlrenderer.pdf.ITextRenderer
+import javax.xml.parsers.{DocumentBuilder,DocumentBuilderFactory}
+
+object PDFPublisher {
+  val documentBuilder = DocumentBuilderFactory.newInstance.newDocumentBuilder
+}
+
+class PDFPublisher(val fileName: String) {
+  val file = new File(fileName)
+  val fileSrc = Source.fromFile(file)
+  val fontStack = new ArrayStack[String]
+  
+  def text = fileSrc.getLines.toList.reduceLeft((a, b) => a + b)
+  def baseURL = file.toURI.toURL.toString
+  def document(fileName: String) = PDFPublisher.documentBuilder.parse(fileName)
+
+  def addFont(fontFileName: String) {
+    fontStack.push(fontFileName)
+  }
+
+  def createPDF(outputFileName: String) {
+    createPDF(outputFileName, baseURL)
+  }
+
+  def createPDF(outputFileName: String, baseURL: String) {
+    val outputStream = new FileOutputStream(outputFileName)
+    val renderer = new ITextRenderer
+    fontStack.foreach(font => renderer.getFontResolver.addFont(font, true))
+    renderer.setDocument(document(fileName), baseURL)
+    renderer.layout
+    renderer.createPDF(outputStream)
+  }
+}
